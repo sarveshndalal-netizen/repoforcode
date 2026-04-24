4edadd1f94f20a843728e70f4a6e9b1362b476ab
sarveshndalal-netizen <sarveshndalal@gmail.com>
2026-04-24 15:48:52 +0530

    Update HelloWorld class to print new messages

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
-class Simple{
-    public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello batch43 team with  Mohit, Yogesh , Sachoin");
+      System.out.println("Hello Batch batch42 WITH FOR STUDENTS RUNNING in front of me and Anurag Prince in making CI automated vikas teach devops CI proecss");
+    }  
+} 
